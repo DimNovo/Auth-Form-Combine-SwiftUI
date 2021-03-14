a9ec637f857712f7ff01414969e1f32634da542b
Dmitry Novosyolov <i1710@icloud.com>
2021-03-14 07:42:40 +0200

    Initial Commit

---
+++ Auth Form Combine SwiftUI/Auth_Form_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_Form_Combine_SwiftUIApp.swift
+//  Auth Form Combine SwiftUI
+//
+//  Created by Dmitry Novosyolov on 14/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Auth_Form_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
